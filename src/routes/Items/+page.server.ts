57b18c168375dc2bd2f1da2c22a548291ca95a07
Matthias Kunnen <matthias.kunnen@gmail.com>
2024-01-14 21:53:28 +0100

    Reduce data size on /Items and /Items/All
    /Items: 371 kB -> 141 kB
    /Items/All: 601 kB -> 230 kB

--- src/routes/Items/+page.server.ts
+++ src/routes/Items/+page.server.ts
@@ -6,6 +6,13 @@ export const load = async () => {
     return {
         items: Object.entries(data.items)
             .filter(([_, item]) => item.recipes.length > 0)
+            .map(([itemId, item]) => [
+                itemId,
+                {
+                    displayName: item.displayName,
+                    icon: item.icon,
+                },
+            ] as const)
             .sort(([, a], [, b]) => {
                 return a.displayName.localeCompare(b.displayName);
             }),
