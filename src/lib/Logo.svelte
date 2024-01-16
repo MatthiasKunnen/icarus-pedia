MatthiasKunnen/icarus-pedia

commit f2623768d59cb77e0221a2a4f0f16475c8a64311

Improve footer; style, responsive, add GitHub link

src/lib/Logo.svelte
@@ -32,7 +32,8 @@
     }
 
     .logo img {
-        height: 3.125em;
+        display: block;
+        height: var(--logo-height);
         width: auto;
     }
 </style>

src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import {afterNavigate} from '$app/navigation';
-    import {page} from '$app/stores';
     import Logo from '$lib/Logo.svelte';
     import type {Snapshot} from '@sveltejs/kit';
 
@@ -8,9 +7,11 @@
     import './global.css';
 
     let rootContent: HTMLElement;
+    let mobileFooterOpen = false;
     let lastScrollRestoreTime = 0;
 
     afterNavigate(() => {
+        mobileFooterOpen = false;
         if (Date.now() - lastScrollRestoreTime > 500) {
             // Only scroll to top if no restore has been performed. This prevents content from
             // flashing on back button where first, a scroll to top is performed, and then
@@ -32,18 +33,45 @@
     };
 </script>
 
-<div class="body">
+<div class="body" class:mobileFooterOpen>
     <div class="root-content" bind:this={rootContent}>
         <slot></slot>
     </div>
+    <div
+        class="mobile-nav-overlay"
+        aria-hidden="true"
+        on:click={() => mobileFooterOpen = false}></div>
     <footer>
+        <nav class="nav-mobile">
+            <a href="/Items">Items</a>
+            <a href="/Crafters">Crafters</a>
+            <a href="https://github.com/MatthiasKunnen/icarus-pedia">
+                <svg class="text-icon" viewBox="0 0 98 96">
+                    <path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/>
+                </svg>
+                GitHub
+            </a>
+        </nav>
         <a href="/" class="logo">
             <Logo size={64}></Logo>
         </a>
-        <nav>
+        <nav class="nav-desktop">
             <a href="/Items">Items</a>
             <a href="/Crafters">Crafters</a>
+            <a href="https://github.com/MatthiasKunnen/icarus-pedia">
+                <svg class="text-icon" viewBox="0 0 98 96">
+                    <path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/>
+                </svg>
+            </a>
         </nav>
+        <button
+            class="nav-toggle"
+            on:click={() => mobileFooterOpen = !mobileFooterOpen}
+        >
+            <svg class="text-icon" viewBox="0 -960 960 960">
+                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+            </svg>
+        </button>
     </footer>
 </div>
 
@@ -64,28 +92,93 @@
         scrollbar-gutter: stable both-edges;
     }
 
+    .mobile-nav-overlay {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: none;
+        background-color: rgba(0, 0, 0, 0.56);
+        cursor: pointer;
+    }
+
     footer {
+        position: relative;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 8px;
+        padding: 4px 8px;
+        background-color: var(--accent-color);
+        --logo-height: 3em;
+    }
+
+    footer .nav-mobile {
+        position: absolute;
+        bottom: 100%;
+        right: 0;
+        left: 0;
+        display: none;
+        flex-direction: column;
         background-color: var(--accent-color);
     }
 
-    footer nav {
+    footer .nav-mobile a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.2em;
+        padding: 0.5em;
+        font-size: 1.5rem;
+        font-weight: bold;
+        border-bottom: 1px solid white;
+        text-align: center;
+    }
+
+    .nav-desktop {
+        display: flex;
         gap: 4px;
     }
 
+    .nav-desktop a {
+        border-radius: 4px;
+    }
+
     footer nav a {
-        border: 1px solid white;
-        border-radius: 8px;
-        padding: 0.25em 1.5em;
+        display: flex;
+        align-items: center;
+        padding: 0.25em 1em;
         font-size: 1.5em;
         font-weight: bold;
-        transition: opacity linear 150ms;
+        transition: background-color linear 150ms;
     }
 
     footer nav a:hover {
-        opacity: 0.5;
+        background-color: rgb(255 255 255 / 22%);
+    }
+
+    .nav-toggle {
+        font-size: 2em;
+        padding: 0 0.3em;
+        display: none;
+    }
+
+    @media (max-width: 599px) {
+
+        .nav-toggle {
+            display: block;
+        }
+
+        .body.mobileFooterOpen .mobile-nav-overlay {
+            display: block;
+        }
+
+        .body.mobileFooterOpen .nav-mobile {
+            display: flex;
+        }
+
+        .nav-desktop {
+            display: none;
+        }
     }
 </style>

src/routes/global.css
@@ -49,3 +49,9 @@ ul.list {
 .box {
     border-top: none;
 }
+
+.text-icon {
+    display: block;
+    height: 1em;
+    width: auto;
+}

src/routes/normalize.css
@@ -23,6 +23,7 @@
     big,
     blockquote,
     body,
+    button,
     canvas,
     caption,
     center,
@@ -179,4 +180,11 @@ table {
 [aria-disabled='true'],
 [disabled] {
     cursor: not-allowed;
+}
+
+:where(button) {
+    background: none;
+    border: none;
+    color: inherit;
+    outline: none;
 }

commit 567d0b4d9f1c47e329cc455c6c078d416303e901

Fix logo height on /
Bug introduced in f2623768d59cb77e0221a2a4f0f16475c8a64311.

src/lib/Logo.svelte
@@ -33,7 +33,7 @@
 
     .logo img {
         display: block;
-        height: var(--logo-height);
+        height: 3em;
         width: auto;
     }
 </style>

src/routes/+layout.svelte
@@ -110,7 +110,6 @@
         align-items: center;
         padding: 4px 8px;
         background-color: var(--accent-color);
-        --logo-height: 3em;
     }
 
     footer .nav-mobile {