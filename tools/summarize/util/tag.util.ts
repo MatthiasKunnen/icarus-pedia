e0c8c03ceb520a545388fb26b8ef8ecc939e7c5c
Matthias Kunnen <matthias.kunnen@gmail.com>
2026-01-04 00:55:50 +0000

    Improve performance of staticItemTagMatches
    For all 2435 items, Manual_Tags contains, in the same order, the first
    x tags of Generated_Tags. This makes Manual_Tags completely redundant.
    It does not even contain tags that are not in Generated_Tags.
    For now, let's keep checking it but do it only if no match can be found
    in the Generated_Tags, which speeds up the fast path.

--- tools/summarize/util/tag.util.ts
+++ tools/summarize/util/tag.util.ts
@@ -4,16 +4,20 @@ export function staticItemTagMatches(
     item: ItemStaticRow,
     predicate: (tagname: string) => boolean,
 ): boolean {
-    if (item.Manual_Tags !== undefined) {
-        for (const gameplayTag of item.Manual_Tags.GameplayTags) {
+    if (item.Generated_Tags !== undefined) {
+        for (const gameplayTag of item.Generated_Tags.GameplayTags) {
             if (predicate(gameplayTag.TagName)) {
                 return true;
             }
         }
     }
 
-    if (item.Generated_Tags !== undefined) {
-        for (const gameplayTag of item.Generated_Tags.GameplayTags) {
+    // For all 2435 items, Manual_Tags contains, in the same order, the first
+    // x tags of Generated_Tags. This makes Manual_Tags completely redundant.
+    // It does not even contain tags that are not in Generated_Tags.
+    // We'll check it just in case.
+    if (item.Manual_Tags !== undefined) {
+        for (const gameplayTag of item.Manual_Tags.GameplayTags) {
             if (predicate(gameplayTag.TagName)) {
                 return true;
             }
