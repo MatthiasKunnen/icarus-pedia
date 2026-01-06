MatthiasKunnen/icarus-pedia

commit 0588bf7a0ee5fcd28634d4f767949d178d137f5f

Improve logging and error handling in summarize/sync

tools/summarize/sync-icons.ts
@@ -2,97 +2,109 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import {fileURLToPath} from 'url';
 
+import {LogWriter} from './util/logwriter.js';
 import type {GameData} from '../../src/lib/data.interface.js';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const outputPath = path.join(dirname, '..', '..', 'static', 'gameicons');
 const sourcePath = path.join(dirname, '..', '..', 'gamedata', 'UI');
+const log = new LogWriter(path.join(dirname, 'sync-icons.ts'));
 
-const data: GameData = JSON.parse(fs.readFileSync(
-    path.join(dirname, 'summarized-data.json'),
-    {encoding: 'utf-8'},
-));
-const icons = [
-    ...Object.values(data.items).map(item => item.icon),
-    ...Object.values(data.crafters).map(item => item.icon),
-];
-icons.push('Logos/Icon_Icarus');
+(async () => {
+    const data: GameData = JSON.parse(fs.readFileSync(
+        path.join(dirname, 'summarized-data.json'),
+        {encoding: 'utf-8'},
+    ));
+    const icons = [
+        ...Object.values(data.items).map(item => item.icon),
+        ...Object.values(data.crafters).map(item => item.icon),
+    ];
+    icons.push('Logos/Icon_Icarus');
 
-console.log('Cleaning output path');
-for (const filename of await fs.promises.readdir(outputPath)) {
-    const filepath = path.join(outputPath, filename);
-    await fs.promises.rm(filepath, {
-        recursive: true,
-    });
-}
-
-const linkedIcons: Array<string> = [];
-console.log('Creating symlinks');
-for (const icon of icons) {
-    await fs.promises.mkdir(path.join(outputPath, path.dirname(icon)), {
-        recursive: true,
-    });
-    const iconSourcePath = `${path.join(sourcePath, icon)}.png`;
-    const iconOutputPath = path.join(outputPath, icon);
-    try {
-        await fs.promises.access(iconSourcePath, fs.constants.R_OK);
-    } catch (error) {
-        console.log(`Skipping "${icon}", it could not be accessed: ${error}`);
-        continue;
+    log.print('Cleaning output path');
+    for (const filename of await fs.promises.readdir(outputPath)) {
+        const filepath = path.join(outputPath, filename);
+        await fs.promises.rm(filepath, {
+            recursive: true,
+        });
     }
 
-    try {
-        await fs.promises.symlink(
-            path.relative(path.dirname(iconOutputPath), `${iconSourcePath}`),
-            `${iconOutputPath}.png`,
-        );
-    } catch (error) {
-        if (error != null
-            && typeof error === 'object'
-            && 'code' in error
-            && typeof error.code === 'string'
-            && error.code === 'EEXIST') {
+    const linkedIcons: Array<string> = [];
+    log.print('Creating symlinks');
+    for (const icon of icons) {
+        await fs.promises.mkdir(path.join(outputPath, path.dirname(icon)), {
+            recursive: true,
+        });
+        const iconSourcePath = `${path.join(sourcePath, icon)}.png`;
+        const iconOutputPath = path.join(outputPath, icon);
+        try {
+            await fs.promises.access(iconSourcePath, fs.constants.R_OK);
+        } catch (error) {
+            log.print(`Skipping "${icon}", it could not be accessed: ${error}`);
             continue;
-        } else {
-            throw error;
         }
+
+        try {
+            await fs.promises.symlink(
+                path.relative(path.dirname(iconOutputPath), `${iconSourcePath}`),
+                `${iconOutputPath}.png`,
+            );
+        } catch (error) {
+            if (error != null
+                && typeof error === 'object'
+                && 'code' in error
+                && typeof error.code === 'string'
+                && error.code === 'EEXIST') {
+                continue;
+            } else {
+                throw error;
+            }
+        }
+
+        linkedIcons.push(iconOutputPath);
     }
 
-    linkedIcons.push(iconOutputPath);
-}
+    const sizes = [
+        '64x64',
+        '128x128',
+    ];
 
-const sizes = [
-    '64x64',
-    '128x128',
-];
+    const commands: Array<string> = [];
+    for (const iconPath of linkedIcons) {
+        commands.push(pngToAvif(iconPath));
+        commands.push(pngToWebp(iconPath));
+        commands.push(pngToJxl(iconPath));
 
-const commands: Array<string> = [];
-for (const iconPath of linkedIcons) {
-    commands.push(pngToAvif(iconPath));
-    commands.push(pngToWebp(iconPath));
-    commands.push(pngToJxl(iconPath));
-
-    for (const size of sizes) {
-        const f = `${iconPath}_${size}`;
-        commands.push(`magick ${iconPath}.png -resize ${size} \
+        for (const size of sizes) {
+            const f = `${iconPath}_${size}`;
+            commands.push(`magick ${iconPath}.png -resize ${size} \
 -define png:exclude-chunk=TIME \
 -strip \
 ${f}.png \
 && ${pngToAvif(f)} \
 && ${pngToWebp(f)} \
 && ${pngToJxl(f)}`);
+        }
     }
-}
 
-const commandOutputFileName = 'image-convert-commands.txt';
-fs.writeFileSync(
-    path.join(dirname, '..', '..', commandOutputFileName),
-    `${commands.join('\n')}\n`,
-    {encoding: 'utf-8'},
-);
+    const commandOutputFileName = 'image-convert-commands.txt';
+    fs.writeFileSync(
+        path.join(dirname, '..', '..', commandOutputFileName),
+        `${commands.join('\n')}\n`,
+        {encoding: 'utf-8'},
+    );
 
-console.log(`Generate the image variants by executing \`parallel --progress < ${
-    commandOutputFileName}\`. Requires GNU parallel.`);
+    log.print(`Generate the image variants by executing \`parallel --progress < ${
+        commandOutputFileName}\`. Requires GNU parallel.`);
+})().catch(async err => {
+    if (err instanceof Error) {
+        await log.fatal(err.toString());
+    } else {
+        await log.fatal(String(err));
+    }
+
+    process.exit(1);
+});
 
 function pngToAvif(iconPath: string): string {
     return `avifenc --jobs 1 --speed 3 -q 50 ${iconPath}.png ${iconPath}.avif > /dev/null`;

.eslintrc.yaml
@@ -30,7 +30,6 @@ overrides:
         - tools/tsconfig.json
     rules:
       '@typescript-eslint/no-var-requires': off
-      no-console: off
 
   -
     files:

tools/summarize/summarize-game-data.ts
@@ -2,28 +2,16 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import {fileURLToPath} from 'url';
 
-import type {RefWithDataTable} from './common.interface.js';
-import type {ConsumableFile, ConsumableRow} from './consumable.interface.js';
+import type {ConsumableFile} from './consumable.interface.js';
 import type {ItemsStatic} from './item-static.interface.js';
 import type {ItemTemplates} from './item-templates.interface.js';
 import type {Itemable} from './itemable.interface.js';
-import type {ModifierStateRow, ModifierStatesFile} from './modifier-states.interface.js';
-import type {ElementCount, ProcessorRecipes} from './processor-recipes.interface.js';
+import type {ModifierStatesFile} from './modifier-states.interface.js';
+import type {ProcessorRecipes} from './processor-recipes.interface.js';
 import type {RecipeSets} from './recipe-sets.interface.js';
 import type {StatsFile} from './stats.interface.js';
-import {extractTranslation} from './util/localization.util.js';
-import {sortObjectKeys} from './util/object.util.js';
-import {extractStats, getModifier} from './util/stats.util.js';
-import {staticItemTagMatches} from './util/tag.util.js';
-import type {
-    Crafter,
-    GameData,
-    ItemCount,
-    ItemStats,
-    Item as OutputItem,
-    Recipe as OutputRecipe,
-    Stat,
-} from '../../src/lib/data.interface.js';
+import {LogWriter} from './util/logwriter.js';
+import {summarizeData} from './util/summarize.js';
 
 /*
  * Maps the data found in the game's several json files and massage them to something smaller we can
@@ -41,453 +29,42 @@ async function readData(filepath: string): Promise<any> {
     ), {encoding: 'utf-8'}));
 }
 
-const itemsStatic: ItemsStatic = await readData('Items/D_ItemsStatic.json');
-const itemTemplates: ItemTemplates = await readData('Items/D_ItemTemplate.json');
-const consumables: ConsumableFile = await readData('Traits/D_Consumable.json');
-const modifiers: ModifierStatesFile = await readData('Modifiers/D_ModifierStates.json');
-const statsFile: StatsFile = await readData('Stats/D_Stats.json');
+const logWriter = new LogWriter(path.join(dirname, 'summarized-data.log'));
 
-/**
- * Contains name, description, and icon link of an item.
- */
-const itemables: Itemable = await readData('Traits/D_Itemable.json');
-const processorRecipes: ProcessorRecipes = await readData('Crafting/D_ProcessorRecipes.json');
-const recipeSets: RecipeSets = await readData('Crafting/D_RecipeSets.json');
-
-const iconPrefix = '/Game/Assets/2DArt/UI/';
-function processIcon(icon: string): string {
-    icon = icon.substring(iconPrefix.length);
-
-    const iconBaseNameIndex = icon.lastIndexOf('/');
-    if (iconBaseNameIndex < 0) {
-        throw new Error('Icon is not a path, no /.');
-    }
-
-    const iconBaseSplit = icon.substring(iconBaseNameIndex + 1).split('.');
-
-    if (iconBaseSplit.length < 2 || iconBaseSplit.length > 2) {
-        throw new Error(`Basename of icon is not split by . as expected. ${icon}`);
-    }
-
-    if (iconBaseSplit[0] !== iconBaseSplit[1]) {
-        throw new Error(`Basename of icon is not symmetrical. ${icon} (${
-            iconBaseSplit[0]} !== ${iconBaseSplit[1]})`);
-    }
-
-    return `${icon.substring(0, iconBaseNameIndex)}/${iconBaseSplit[0]}`;
-}
-
-/**
- * Some items are named with different case in the recipes. This maps exists to retrieve the correct
- * case. The key is from D_ItemsStatic.json, lowercase. The value is the original case.
- */
-const itemStaticCaseCorrectionMap = new Map<string, string>();
-
-/**
- * Maps item templates to their static name.
- */
-const itemTemplateMap = new Map<string, string>();
-
-for (const itemTemplate of itemTemplates.Rows) {
-    if (itemTemplate.ItemStaticData === undefined) {
-        continue;
-    }
-
-    itemTemplateMap.set(itemTemplate.Name.toLowerCase(), itemTemplate.ItemStaticData.RowName);
-}
-
-const consumableMap = new Map<string, ConsumableRow>();
-for (const consumable of consumables.Rows) {
-    consumableMap.set(consumable.Name, consumable);
-}
-
-const modifierStatesMap = new Map<string, ModifierStateRow>();
-for (const modifierState of modifiers.Rows) {
-    modifierStatesMap.set(modifierState.Name, modifierState);
-}
-
-function getItemStaticName(ref: RefWithDataTable): string | undefined {
-    let result: string | undefined;
-
-    switch (ref.DataTableName) {
-        case 'D_ItemTemplate':
-            result = itemTemplateMap.get(ref.RowName.toLowerCase());
-            break;
-        case 'D_ItemsStatic':
-            result = ref.RowName;
-            break;
-        default:
-            console.log(`Unknown datatable name ${ref.DataTableName}`);
-            return undefined;
-    }
-
-    if (result !== undefined) {
-        return itemStaticCaseCorrectionMap.get(result.toLowerCase());
-    }
-
-    return result;
-}
-
-// Stats used by items in output.
-const statsToInclude = new Set<string>();
-const stats = new Map<string, Stat>();
-for (const statRow of statsFile.Rows) {
-    const positiveFormat = extractTranslation(statRow.PositiveDescription);
-    const negativeFormat = extractTranslation(statRow.NegativeDescription);
-
-    if (positiveFormat === undefined && negativeFormat !== undefined) {
-        console.warn(`Stat '${statRow.Name}' has only negative description.`);
-    }
-
-    if (positiveFormat === undefined) {
-        continue;
-    }
-
-    stats.set(statRow.Name, {
-        positiveFormat: positiveFormat,
-        negativeFormat: negativeFormat,
-    });
-}
-
-const itemBlacklist: Array<string> = [
-    'CollectionShipBeacon',
-    'Cooking_Station', // Replaced with _V2
-    'DEV_Bug_Tool',
-    'DEV_Fireball',
-    'DEV_Inspection_Tool',
-    'DEV_Thor_Hammer',
-    'Debug_Target',
-    'Electric_Dehumidifier', // Replaced with _V2
-    'Faction_MIssion_Analyzer',
-    'Faction_Mission_Frozen_Mammoth_Sample',
-    'Faction_Mission_Mammoth_Sample',
-    'Faction_Satellite',
-    'Faction_Satellite_Defend',
-    'Farming_CropPlot', // Replaced with _T2_V2
-    'Farming_CropPlot_T3', // Replaced with _v2
-    'Farming_CropPlot_T4', // Replaced with _v2
-    'Glassworking_Bench', // Replaced with _v2
-    'Kit_Road',
-    'Player_Gravestone_DBNO',
-    'Player_Gravestone_MIA',
-    'SplineTool_Fuel',
-    'Water_Purifier_T1',
-];
-
-const itemExcluded: Record<string, string> = {};
-const mappedItems: Record<string, OutputItem> = {};
-for (const item of itemsStatic.Rows) {
-    itemStaticCaseCorrectionMap.set(item.Name.toLowerCase(), item.Name);
-    if (item.Itemable === undefined) {
-        itemExcluded[item.Name] = 'Not itemable';
-        continue;
-    }
+(async () => {
+    const itemsStatic: ItemsStatic = await readData('Items/D_ItemsStatic.json');
+    const itemTemplates: ItemTemplates = await readData('Items/D_ItemTemplate.json');
+    const consumables: ConsumableFile = await readData('Traits/D_Consumable.json');
+    const modifiers: ModifierStatesFile = await readData('Modifiers/D_ModifierStates.json');
+    const statsFile: StatsFile = await readData('Stats/D_Stats.json');
 
     /**
-     * E.g. Name of item as used in D_ItemsStatic.json, e.g. Stick.
+     * Contains name, description, and icon link of an item.
      */
-    const itemableName = item.Itemable.RowName;
+    const itemables: Itemable = await readData('Traits/D_Itemable.json');
+    const processorRecipes: ProcessorRecipes = await readData('Crafting/D_ProcessorRecipes.json');
+    const recipeSets: RecipeSets = await readData('Crafting/D_RecipeSets.json');
 
-    if (itemBlacklist.includes(item.Name)) {
-        itemExcluded[item.Name] = 'Blacklisted';
-        continue;
-    }
-
-    /*
-    let type: 'Attachment' | 'Consumable' | 'Knife' | undefined;
-    if (item.Consumable !== undefined) {
-        type = 'Consumable';
-    } else if (item.Attachments !== undefined) {
-        type = 'Attachment';
-    } else if (item.Manual_Tags?.GameplayTags.some(t => {
-        return t.TagName.startsWith('Item.Tool.Knife');
-    }) === true) {
-        type = 'Knife';
-    }
-    */
-
-    const itemable = itemables.Rows.find(itemr => {
-        return itemr.Name === itemableName;
+    const gameData = summarizeData({
+        consumables: consumables,
+        itemTemplates: itemTemplates,
+        itemables,
+        itemsStatic: itemsStatic,
+        log: logWriter,
+        modifiers: modifiers,
+        processorRecipes,
+        recipeSets,
+        statsFile,
     });
 
-    if (itemable === undefined) {
-        console.log(`${item.Name} not found in itemable`);
-        itemExcluded[item.Name] = 'Not found in itemable';
-        continue;
-    }
-
-    if (itemable.Icon === undefined) {
-        itemExcluded[item.Name] = 'No icon';
-        continue;
-    } else if (!itemable.Icon.startsWith(iconPrefix)) {
-        // Only used for a single dev items it seems, DEV_Transform_Tool
-        itemExcluded[item.Name] = 'Does not start with iconPrefix';
-        continue;
-    }
-
-    const displayName = extractTranslation(itemable.DisplayName);
-
-    if (displayName === undefined) {
-        itemExcluded[item.Name] = 'No display name';
-        continue;
-    }
-
-    let additionalStats: ItemStats | undefined;
-    if (item.AdditionalStats !== undefined) {
-        const [statsResult, error] = extractStats(item.AdditionalStats, stats);
-        if (error === null) {
-            additionalStats = statsResult;
-        } else {
-            console.error(`AdditionalStats: item=${item.Name}, Err=${error}`);
-        }
-    }
-
-    const consumable = item.Consumable?.RowName === undefined
-        ? undefined
-        : consumableMap.get(item.Consumable.RowName);
-    let consumeStats: ItemStats | undefined;
-    if (consumable?.Stats !== undefined) {
-        const [statsResult, error] = extractStats(consumable.Stats, stats);
-        if (error === null) {
-            consumeStats = statsResult;
-        } else {
-            console.error(`stats: item=${item.Name}, consumable=${consumable.Name}. Err=${error}`);
-        }
-    }
-
-    const [modifier, modifierError] = getModifier(
-        consumable,
-        modifierStatesMap,
-        stats,
+    fs.writeFileSync(
+        path.join(dirname, 'summarized-data.json'),
+        `${JSON.stringify(gameData, undefined, 4)}\n`,
+        {
+            encoding: 'utf-8',
+        },
     );
-    if (modifierError !== null) {
-        console.error(`item=${item.Name}: ${modifierError}`);
-    }
-
-    const itemStats: ItemStats = sortObjectKeys({
-        ...consumeStats,
-        ...additionalStats,
-    });
-    for (const stat of Object.keys(itemStats)) {
-        statsToInclude.add(stat);
-    }
-
-    if (modifier?.stats !== undefined) {
-        for (const stat of Object.keys(modifier.stats)) {
-            statsToInclude.add(stat);
-        }
-    }
-
-    mappedItems[item.Name] = {
-        crafter: item.Processing?.RowName,
-        displayName: displayName,
-        icon: processIcon(itemable.Icon),
-        description: extractTranslation(itemable.Description),
-        flavorText: extractTranslation(itemable.FlavorText),
-        isFood: staticItemTagMatches(item, tag => tag.startsWith('Item.Consumable.Food')),
-        recipes: [],
-        ingredientIn: [],
-        stats: Object.keys(itemStats).length > 0 ? itemStats : undefined,
-        modifier: modifier ?? undefined,
-        usable: undefined,
-    };
-}
-
-// Report reason for item exclusion
-console.log(`Excluded items:`);
-for (const [itemName, exclusionReason] of Object.entries(itemExcluded)) {
-    console.log(`- ${itemName}: ${exclusionReason}`);
-}
-
-const crafters: Record<string, Crafter> = {};
-for (const recipeSet of recipeSets.Rows) {
-    const icon = recipeSet.RecipeSetIcon;
-    const displayName = extractTranslation(recipeSet.RecipeSetName);
-
-    if (icon === undefined || icon === 'None' || displayName === undefined) {
-        continue;
-    }
-
-    crafters[recipeSet.Name] = {
-        icon: processIcon(icon),
-        displayName: displayName,
-        recipes: [],
-    };
-}
-
-const blacklistedRecipeSets: Array<string> = [
-    'Cleaning_Device',
-    'RefundOnly',
-    'T3_Smoker',
-    'T4_Smoker',
-];
-const mappedRecipes: Record<string, OutputRecipe> = {};
-for (const recipe of processorRecipes.Rows) {
-    const elementCountsToItemCount = (elementCounts: Array<ElementCount>): Array<ItemCount> => {
-        const itemCounts: Array<ItemCount> = [];
-
-        for (const elementCount of elementCounts) {
-            const itemName = getItemStaticName(elementCount.Element);
-            if (itemName === undefined) {
-                console.error(`Could not find static name for element`, elementCount.Element);
-                continue;
-            }
-
-            itemCounts.push({
-                item: itemName,
-                count: elementCount.Count,
-            });
-        }
-
-        return itemCounts.sort((a, b) => a.item.localeCompare(b.item));
-    };
-
-    const inputs = elementCountsToItemCount(recipe.Inputs);
-    const outputs = elementCountsToItemCount(recipe.Outputs);
-
-    if (inputs.length !== recipe.Inputs.length || outputs.length !== recipe.Outputs.length) {
-        console.log(`Excluding recipe ${recipe.Name} because not all inputs and outputs could be `
-            + `found.`);
-        continue;
-    }
-
-    let skip = false;
-    for (const recipeInput of inputs) {
-        const mappedItem = mappedItems[recipeInput.item];
-        if (mappedItem === undefined) {
-            const reason = itemExcluded[recipeInput.item];
-            console.log(`Excluding recipe ${recipe.Name} because input ${
-                recipeInput.item} is not mapped.${
-                reason === undefined ? '' : ` Reason: ${reason}.`}`);
-            skip = true;
-            break;
-        }
-        mappedItem.ingredientIn.push(recipe.Name);
-    }
-    for (const recipeOutput of outputs) {
-        const mappedItem = mappedItems[recipeOutput.item];
-        if (mappedItem === undefined) {
-            const reason = itemExcluded[recipeOutput.item];
-            console.log(`Excluding recipe ${recipe.Name} because output ${
-                recipeOutput.item} is not mapped.${
-                reason === undefined ? '' : ` Reason: ${reason}.`}`);
-            skip = true;
-            break;
-        }
-        mappedItem.recipes.push(recipe.Name);
-    }
-    if (skip) {
-        continue;
-    }
-
-    for (const recipeSet of recipe.RecipeSets) {
-        if (recipeSet.DataTableName !== 'D_RecipeSets') {
-            console.error(`Unknown RecipeSet datatable ${recipeSet.DataTableName} for recipe ${
-                recipe.Name}`);
-        }
-
-        if (blacklistedRecipeSets.includes(recipeSet.RowName)) {
-            continue;
-        }
-
-        const crafter = crafters[recipeSet.RowName];
-
-        if (crafter === undefined) {
-            console.log(`Unknown RecipeSet ${recipeSet.RowName} for recipe ${recipe.Name}`);
-            continue;
-        }
-
-        crafter.recipes.push(recipe.Name);
-    }
-
-    mappedRecipes[recipe.Name] = {
-        requirement: recipe.Requirement?.RowName,
-        craftedAt: recipe.RecipeSets.map(recipeSet => {
-            switch (recipeSet.DataTableName) {
-                case 'D_RecipeSets':
-                    return recipeSet.RowName;
-                default:
-                    throw new Error(`Unknown RecipeSet data table name: ${
-                        recipeSet.DataTableName}`);
-            }
-        }).filter(rs => !blacklistedRecipeSets.includes(rs)),
-        inputs: inputs,
-        outputs: outputs,
-    };
-}
-
-for (const statName of stats.keys()) {
-    // Do not include unused stats in result
-    if (statsToInclude.has(statName)) {
-        continue;
-    }
-
-    stats.delete(statName);
-}
-
-// Weed out items that are not craftable/used in recipes
-const usabilityChecked = new Set<string>();
-function markUsableRecursive(itemName: string, item: OutputItem): void {
-    if (usabilityChecked.has(itemName)) {
-        return;
-    }
-    usabilityChecked.add(itemName);
-    if (item.recipes.length > 0 || item.ingredientIn.length > 0) {
-        item.usable = true;
-    }
-
-    for (const recipeName of item.recipes) {
-        const recipe = mappedRecipes[recipeName];
-        if (recipe === undefined) {
-            console.error(`Recipe of item ${itemName} with recipe name ${
-                recipeName} not found in usability check`);
-            continue;
-        }
-        for (const input of recipe.inputs) {
-            const mappedItem = mappedItems[input.item];
-            if (mappedItem === undefined) {
-                console.error(`Recipe input of item ${input.item}, recipe ${
-                    recipeName}, item ${input.item} not found in usability check`);
-                continue;
-            }
-
-            markUsableRecursive(input.item, mappedItem);
-        }
-    }
-}
-for (const [name, item] of Object.entries(mappedItems)) {
-    // First mark everything
-    markUsableRecursive(name, item);
-}
-let usable = 0;
-const unusable: Array<string> = [];
-for (const [itemName, item] of Object.entries(mappedItems)) {
-    if (item.usable === true) {
-        usable++;
-        // Undefined equals usable, this is done to decrease the size of the resulting output
-        item.usable = undefined;
-    } else {
-        item.usable = false;
-        unusable.push(itemName);
-    }
-}
-console.log(`Of the non-excluded items, ${usable} are usable in crafting, the following ${
-    unusable.length} are not:`);
-for (const itemName of unusable) {
-    console.log(`- ${itemName}`);
-}
-
-const gameData: GameData = {
-    crafters: sortObjectKeys(crafters),
-    items: sortObjectKeys(mappedItems),
-    recipes: sortObjectKeys(mappedRecipes),
-    stats: sortObjectKeys(Object.fromEntries(stats)),
-};
-
-fs.writeFileSync(
-    path.join(dirname, 'summarized-data.json'),
-    `${JSON.stringify(gameData, undefined, 4)}\n`,
-    {
-        encoding: 'utf-8',
-    },
-);
+})().catch(async err => {
+    await logWriter.fatal(err.message);
+    process.exit(1);
+});

tools/summarize/util/localization.util.ts
@@ -3,10 +3,12 @@ interface ParseResult {
     arguments: Array<string>;
 }
 
-function parseFunction(text: string): ParseResult | null {
+function parseFunction(
+    text: string,
+): [parsed: null, error: Error] | [parsed: ParseResult, error: null] {
     const openParenIndex = text.indexOf('(');
     if (openParenIndex === -1) {
-        return null;
+        return [null, new Error('No opening parenthese')];
     }
 
     const functionName = text.substring(0, openParenIndex);
@@ -39,7 +41,9 @@ function parseFunction(text: string): ParseResult | null {
                                 i++;
                                 break;
                             default:
-                                console.error(`Unknown escaped char: ${text[i + 1]} in ${text}`);
+                                return [null, new Error(
+                                    `Unknown escaped char: ${text[i + 1]} in ${text}`,
+                                )];
                         }
                         break;
                     case '"':
@@ -65,36 +69,41 @@ function parseFunction(text: string): ParseResult | null {
                             args.push(argument);
                         }
 
-                        return {
-                            functionName,
-                            arguments: args,
-                        };
+                        return [
+                            {
+                                functionName,
+                                arguments: args,
+                            },
+                            null,
+                        ];
                     default:
                         argument += char;
                 }
         }
     }
 
-    console.error(`Failed to parse function "${text}"`);
-
-    return null;
+    return [null, new Error(`Failed to parse function "${text}"`)];
 }
 
-export function extractTranslation(fn: string | undefined): string | undefined {
+export function extractTranslation(
+    fn: string | undefined,
+): [string | undefined, null] | [undefined, Error] {
     if (fn === undefined) {
-        return undefined;
+        return [undefined, null];
     }
 
-    const parsed = parseFunction(fn);
-
-    if (parsed === null) {
-        return undefined;
+    const [parsed, err] = parseFunction(fn);
+    if (err !== null) {
+        return [undefined, err];
     }
 
     switch (parsed.functionName) {
         case 'NSLOCTEXT':
-            return parsed.arguments[2];
+            if (parsed.arguments[2] === undefined) {
+                return [undefined, new Error('No third argument in NSLOCTEXT')];
+            }
+            return [parsed.arguments[2], null];
         default:
-            return undefined;
+            return [undefined, null];
     }
 }

tools/summarize/util/logwriter.ts
@@ -1,0 +1,64 @@
+import {createWriteStream, type WriteStream} from 'node:fs';
+
+export class LogWriter {
+    private readonly fileStream?: WriteStream;
+
+    constructor(filePath?: string) {
+        if (filePath !== undefined) {
+            this.fileStream = createWriteStream(filePath, {
+                flags: 'w',
+            });
+        }
+    }
+
+    print(message: string): void {
+        process.stdout.write(message, 'utf-8');
+        process.stdout.write('\n', 'utf-8');
+        this.fileStream?.write(message, 'utf-8');
+        this.fileStream?.write('\n', 'utf-8');
+    }
+
+    /**
+     * Logs an error, flushes all streams, and exits the process.
+     */
+    async fatal(message: string): Promise<never> {
+        this.print(`[FATAL] ${message}`);
+        try {
+            await this.flush();
+        } finally {
+            process.exit(1);
+        }
+    }
+
+    /**
+     * Ensures all buffered data is written to the underlying system.
+     */
+    async flush(): Promise<void> {
+        const promises: Array<Promise<unknown>> = [
+            new Promise<void>((resolve) => {
+                process.stdout.write('\n', err => {
+                    // eslint-disable-next-line no-console
+                    console.log('Error flushing stdout buffer', err);
+                    resolve();
+                });
+            }),
+        ];
+        const fileStream = this.fileStream;
+        if (fileStream !== undefined) {
+            promises.push(new Promise<void>((resolve) => {
+                if (fileStream.writableLength === 0) {
+                    resolve();
+                    return;
+                }
+
+                fileStream.once('drain', resolve);
+            }));
+        }
+
+        try {
+            await Promise.allSettled(promises);
+        } catch {
+            // Ignore
+        }
+    }
+}

commit c7301d03a9065db6f0217b2444b6edcea2273ba2

Fix sync-icons.ts overwriting itself
Bug introduced in 0588bf7a0ee5fcd28634d4f767949d178d137f5f.

tools/summarize/sync-icons.ts
@@ -8,7 +8,7 @@ import type {GameData} from '../../src/lib/data.interface.js';
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const outputPath = path.join(dirname, '..', '..', 'static', 'gameicons');
 const sourcePath = path.join(dirname, '..', '..', 'gamedata', 'UI');
-const log = new LogWriter(path.join(dirname, 'sync-icons.ts'));
+const log = new LogWriter(path.join(dirname, 'sync-icons.log'));
 
 (async () => {
     const data: GameData = JSON.parse(fs.readFileSync(