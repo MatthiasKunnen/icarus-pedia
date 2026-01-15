c0f06d2378114a2df70618444b7d4a1f3b01d1df
Matthias Kunnen <matthias.kunnen@gmail.com>
2026-01-15 16:01:22 +0100

    Improve landings page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,16 @@
     <div class="logo">
         <Logo size="auto"></Logo>
     </div>
-    <p>An information hub on the items in Icarus and how they are crafted.</p>
     <p>
-        Content is extracted from the game files. This makes sure the information is up-to-date
-        though items that are not yet accessible may be shown.
+        An information hub on the items in Icarus.
+        All data is extracted from the game files
+        This makes sure the information is up-to-date, though items that are not yet accessible may
+        be shown.
     </p>
-    <p>
-    </p>
+    <h2>Items</h2>
+    <p>View all items in Icarus at <a class="link" href="/Items">/Items</a>.</p>
+    <h2>Crafting benches</h2>
+    <p>View all crafting benches <a class="link" href="/Crafters">/Crafters</a>.</p>
 </div>
 
 <style>
@@ -26,7 +29,7 @@
         align-items: center;
         justify-content: center;
         gap: 1em;
-        font-size: min(5vw, 2em);
+        font-size: min(5vw, 1.5em);
         text-align: center;
     }
 
